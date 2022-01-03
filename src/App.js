cd7d083a4adbfe194a296e91a6bda2224b5ef924
Udo-Eng <udochukwuabazie.soft@gmail.com>
2022-01-03 20:50:18 +0100

    I remove clarifai form the frontEnd application

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ import SignIn from './components/SignIn/SignIn.js';
 import Register from './components/Register/Register.js';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition.js';
 import './App.css';
-// import Particles from 'react-particles-js';
-import API_KEY from './apiKeys';
-import Clarifai from 'clarifai';
+// // import Particles from 'react-particles-js';
+// import API_KEY from './apiKeys';
+// import Clarifai from 'clarifai';
 
 
-//Initailize a new clarifai app instance 
-const app = new Clarifai.App({
-  apiKey: `${API_KEY}`
-});
+// //Initailize a new clarifai app instance 
+// const app = new Clarifai.App({
+//   apiKey: `${API_KEY}`
+// });
 
 
 // const ParticleOptions = {
@@ -135,25 +135,32 @@ class App extends Component {
     this.setState({
       imageURL: this.state.input,
     });
-    app.models
-      .predict(
-        // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
-        // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
-        // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
-        // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
-        // is to use a different version of their model that works like the ones found here: https://github.com/Clarifai/clarifai-javascript/blob/master/src/index.js
-        // so you would change from:
-        // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-        // to:
-        // .predict('53e1df302c079b3db8a0a36033ed2d15', this.state.input)
-        // '53e1df302c079b3db8a0a36033ed2d15',
-        Clarifai.FACE_DETECT_MODEL,
-        this.state.input)
-      // .then(data => {
-      //   this.displayFaceBox(this.calculateFaceLocation(data.outputs[0].data.regions[0].region_info.bounding_box));
-      // })
+    // app.models
+    //   .predict(
+    //     // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
+    //     // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
+    //     // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
+    //     // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
+    //     // is to use a different version of their model that works like the ones found here: https://github.com/Clarifai/clarifai-javascript/blob/master/src/index.js
+    //     // so you would change from:
+    //     // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    //     // to:
+    //     // .predict('53e1df302c079b3db8a0a36033ed2d15', this.state.input)
+    //     // '53e1df302c079b3db8a0a36033ed2d15',
+    //     Clarifai.FACE_DETECT_MODEL,
+    //     this.state.input)
+    // .then(data => {
+    //   this.displayFaceBox(this.calculateFaceLocation(data.outputs[0].data.regions[0].region_info.bounding_box));
+    // })
+    fetch('http://localhost:5000/image', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    }).then(response => response.json())
       .then(response => {
-        console.log('hi', response)
+
         if (response) {
           fetch('http://localhost:5000/image', {
             method: 'put',
